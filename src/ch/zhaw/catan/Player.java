443f3d73e3016ff1d2f004ab3e02c9e0bc7272f6
Sinrath <phi7po@gmail.com>
2021-11-28 20:38:53 +0100

    added player

--- src/ch/zhaw/catan/Player.java
+++ src/ch/zhaw/catan/Player.java
@@ -1,8 +1,35 @@
 package ch.zhaw.catan;
 
 import ch.zhaw.catan.Config.Faction;
+import ch.zhaw.catan.Config.Resource;
+import java.util.HashMap;
 
 public class Player {
-    private Faction faction;
+    private boolean playerActive;
+    private final Faction playerFaction;
+    private HashMap<Resource, Integer> playerResource;
+
+    public Player(Faction PlayerFaction) {
+        playerActive = true;
+        this.playerFaction = PlayerFaction;
+        playerResource = new HashMap<>();
+    }
+
+    public boolean getPlayerActive() {
+        return playerActive;
+    }
+
+    public Faction getPlayerFaction() {
+        return playerFaction;
+    }
+
+    public int getPlayerResource(Resource resource) {
+        return playerResource.get(resource);
+
+    }
+
+    private void setPlayerResource() {
+        //TODO
+    }
 
 }
